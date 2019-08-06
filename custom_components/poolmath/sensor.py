da490d7edfbc7b2bb18b34902630848e107673a7
Ryan Snodgrass <rsnodgrass@gmail.com>
2019-08-05 18:11:57 -0700

    update

--- custom_components/poolmath/sensor.py
+++ custom_components/poolmath/sensor.py
@@ -39,78 +39,51 @@ def setup_platform(hass, config, add_entities, discovery_info=None):
     """Set up the Web scrape sensor."""
     url = config.get(CONF_RESOURCE)
 
-    rest = RestData('GET', url, '', '', '', verify_ssl)
-    rest.update()
-
-    if rest.data is None:
-        raise PlatformNotReady
-
-    raw_data = BeautifulSoup(self.rest.data, 'html.parser')
-    log.debug(raw_data)
-
-    # FIXME: if Default Name...append the "name" from the URL reply under <a name="NAME"><h1>NAME</h1></a>
-    url_name = raw_data.select('body.h1').string
-    log.info(f"Loaded Pool Math for '{url_name}'")
-
-    name = config.get(CONF_NAME)
-    if name == None:
-        name = DEFAULT_NAME + " " + url_name 
-    log.info(f"Pool Math sensor name: {name}")
-
-    #select_one(".testLogCard")
-    most_recent_test_log = raw_data.find('div', class_='testLogCard')
-    log.info(f"Most recent test log entry: {most_recent_test_log}")
-
-    if most_recent_test_log == None:
-        log.info(f"Couldn't find any test logs at {url}")
-        raise PlatformNotReady
-
-    # capture the time the most recent Pool Math data was collected
-    timestamp = most_recent_test_log.find('time', class_='timestamp timereal')
-    log.info(f"Timestamp for most recent test log: {timestamp}")
-
-    # iterate through all the data chiclets and dynamically create sensors
-
-    data_entries = most_recent_test_log.find(class_='chiclet')
-    for entry in data_entries:
-        #bold is data... other is which sensor result
-        log.info(f"Entry = {entry}")
+    client = PoolMathClient(config)
 
     # FIXME: create a sensor for each value?
 #    add_entities(
 #        [PoolMathSensor(rest, name, select, attr, index, value_template, unit)], True
 #    )
 
-# FIXME: don't update more frequently than a configured interval
 class PoolMathClient():
     def __init__(self, config):
         verify_ssl = False
 
         self._url = config.get(CONF_URL)
         self._rest = RestData('GET', self._url, '', '', '', verify_ssl)
-        self._rest.update()
 
+        self._rest.update()
         if self._rest.data is None:
             raise PlatformNotReady
 
-    def update():
-        self._url = config.get(CONF_URL)
-        self._rest = RestData('GET', self._url, '', '', '', verify_ssl)
-        self._rest.update()
-
-        raw_data = BeautifulSoup(self.rest.data, 'html.parser')
-        log.debug(raw_data)
-
-        # FIXME: if Default Name...append the "name" from the URL reply under <a name="NAME"><h1>NAME</h1></a>
-        url_name = raw_data.select('body.h1').string
-        log.info(f"Loaded Pool Math for '{url_name}'")
+        self._raw_data = BeautifulSoup(self._rest.data, 'html.parser')
 
         self._name = config.get(CONF_NAME)
         if self._name == None:
-            self._name = DEFAULT_NAME + " " + url_name 
-        log.info(f"Pool Math sensor name: {name}")
+            self._name = DEFAULT_NAME
 
-        #select_one(".testLogCard")
+            pool_name = raw_data.select('body.h1').string
+            if pool_name != None:
+                self._name += " " + pool_name
+                log.info(f"Loaded Pool Math data for '{pool_name}'")
+
+        log.info(f"Created Pool Math sensor: {self._name}")
+        self._sensors = {}
+        self._update_sensors()
+
+    # FIXME: don't update more frequently than a configured interval
+    
+    def update():
+        self._rest.update()
+        if self._rest.data is None:
+            log.warning(f"Failed to update Pool Math data for '{self._name}' from {self._url}")
+            return
+
+        self._raw_data = BeautifulSoup(self._rest.data, 'html.parser')
+        self._update_sensors()
+
+    def _update_sensors():
         most_recent_test_log = raw_data.find('div', class_='testLogCard')
         log.info(f"Most recent test log entry: {most_recent_test_log}")
 
@@ -129,55 +102,8 @@ class PoolMathClient():
             log.info(f"Entry = {entry}")
             sensor_type = 'fc'
 
-        # FIXME: create a sensor for each value?  
-        #    add_entities(
-        #        [UpdatableSensor(self, name, unit)], True
-        #    )
-
-
-        self._entities = []
-        self._name = DEFAULT_NAME
-
-        self._rest = RestData('GET', url, '', '', '', verify_ssl)
-        self._rest.update()
-
-        if self._rest.data is None:
-            raise PlatformNotReady
-
     def get_name(self):
         return self._name 
-
-    def update(self):
-        """Get the latest data from the source and update all the corresponding sensors"""
-        self.rest.update()
-        if self.rest.data is None:
-            log.error("Unable to retrieve data")
-            return
-
-        raw_data = BeautifulSoup(self.rest.data, "html.parser")
-        log.debug(raw_data)
-
-        self._last_data = raw_data
-
-        name = raw_data.select('.h1.string')
-        log.info("Loaded Pool Math for '{name}'")
-
-        try:
-            if self._attr is not None:
-                value = raw_data.select(self._select)[self._index][self._attr]
-            else:
-                value = raw_data.select(self._select)[self._index].text
-            log.debug(value)
-        except IndexError:
-            log.error("Unable to extract data from HTML")
-            return
-
-        if self._value_template is not None:
-            self._state = self._value_template.render_with_possible_json_value(
-                value, None
-            )
-        else:
-            self._state = value
 
 class UpdatableSensor(Entity):
     """Representation of a sensor whose state is kept up-to-date by an external data source."""
